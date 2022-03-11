6c4fcab5e6f3e4d0c125d6f40a2128a15bda1125
exlibrischimex <exlibrischimex@gmail.com>
2022-03-11 16:48:19 +0000

    Unix is basically a simple operating system,but you have to be genuis to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
 
 /**
  * main - prints exactly "and that piece of art is useful"
- * -Dora Korpa, 2015-10-19",
+ * - Dora Korpa, 2015-10-19",
  * followed by new line, to the standard error.
  * Return: Always 0 (Success)
  */
