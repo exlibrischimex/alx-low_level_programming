8b55759f3504470925bf8fe36633b27568228cb4
exlibrischimex <exlibrischimex@gmail.com>
2022-03-16 11:08:06 +0000

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -11,14 +11,13 @@ int _isalpha(int c);
 
 {
 
-if ((c > 'a' && c <= 'z')) || (c > 'A' && c < 'z'))
+if ((c > 'a' && c <= 'z')) || (c > 'A' && c < 'Z'))
 {
 return (1);
 }
 
 else
 {
-
 return (0);
 }
 
