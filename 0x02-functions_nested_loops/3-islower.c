1b2f3c4c0356bf0fc0a688476072f38b90d17eec
exlibrischimex <exlibrischimex@gmail.com>
2022-03-16 09:59:32 +0000

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * _islower - Short description, single line
+ * _islower - returns 1 if lowercase alphabet and 0 otherwise
  * @c: contains value to be compared
  * Return: Always 0.
  */
 int _islower(int c)
-
 {
 
 if (c >'a' && c < 'z')
 {
-return (1)
+return (1);
 }
 
 else
 {
+
 return (0);
 }
 
